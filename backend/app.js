5c9090549d083728eb011e36a6e22b9c87f96f29
Luis Nickel <wqe589@haw-hamburg.de>
2024-06-28 07:10:17 +0200

    Added basic UI components and auth route

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const authRoutes = require('./routes/auth');
 
 app.use(express.json());
+app.use('/auth', authRoutes);
 
-// Define routes
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
